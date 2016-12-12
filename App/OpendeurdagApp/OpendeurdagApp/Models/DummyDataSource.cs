a8097ea362a3259f0b91ee266f81df89174da9dc
JoritVergalle <jorit.vergalle@hotmail.com>
2016-12-12 12:54:09 +0100

    Nieuwspagina maar binding werkt nog niet

---
+++ App/OpendeurdagApp/OpendeurdagApp/Models/DummyDataSource.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpendeurdagApp.Models
+{
+    public static class DummyDataSource
+    {
+        public static List<News> News { get; set; } = new List<News>()
+        {
+            new News() {Title="Aankoop Hololens",Description="Er is eindelijk beslist om een hololens aan te kopen."}
+            
+    };
+}
+}
---
+++ App/OpendeurdagApp/OpendeurdagApp/Models/News.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpendeurdagApp.Models
+{
+    public class News : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void RaisePropertyChanged([CallerMemberName]string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private String title;
+
+        public String Title
+        {
+            get { return title; }
+            set { title = value; RaisePropertyChanged(); }
+        }
+
+        private String description;
+
+        public String Description
+        {
+            get { return description; }
+            set { description = value; RaisePropertyChanged(); }
+        }
+    }
+}
