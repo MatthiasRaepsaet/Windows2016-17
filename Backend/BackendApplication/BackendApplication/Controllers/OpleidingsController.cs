4c1b3fc5014c8ee6896c64da67e7a4218f61960e
Matthias Raepsaet <Matthias Raepsaet>
2016-12-17 14:44:05 +0100

    backendcode

---
+++ Backend/BackendApplication/BackendApplication/Controllers/OpleidingsController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using BackendApplication.Models;
+
+namespace BackendApplication.Controllers
+{
+    public class OpleidingsController : ApiController
+    {
+        private BackendApplicationContext db = new BackendApplicationContext();
+
+        // GET: api/Opleidings
+        public IQueryable<Opleiding> GetOpleidings()
+        {
+            return db.Opleidings;
+        }
+
+        // GET: api/Opleidings/5
+        [ResponseType(typeof(Opleiding))]
+        public IHttpActionResult GetOpleiding(int id)
+        {
+            Opleiding opleiding = db.Opleidings.Find(id);
+            if (opleiding == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(opleiding);
+        }
+
+        // PUT: api/Opleidings/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutOpleiding(int id, Opleiding opleiding)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != opleiding.Id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(opleiding).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!OpleidingExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Opleidings
+        [ResponseType(typeof(Opleiding))]
+        public IHttpActionResult PostOpleiding(Opleiding opleiding)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Opleidings.Add(opleiding);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = opleiding.Id }, opleiding);
+        }
+
+        // DELETE: api/Opleidings/5
+        [ResponseType(typeof(Opleiding))]
+        public IHttpActionResult DeleteOpleiding(int id)
+        {
+            Opleiding opleiding = db.Opleidings.Find(id);
+            if (opleiding == null)
+            {
+                return NotFound();
+            }
+
+            db.Opleidings.Remove(opleiding);
+            db.SaveChanges();
+
+            return Ok(opleiding);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool OpleidingExists(int id)
+        {
+            return db.Opleidings.Count(e => e.Id == id) > 0;
+        }
+    }
+}
---
+++ Backend/BackendApplication/BackendApplication/Models/Gebruiker.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BackendApplication.Models
+{
+    public class Gebruiker
+    {
+        public int Id { get; set; }
+        public string Voornaam { get; set; }
+        public string Achternaam { get; set; }
+        public string Email { get; set; }
+
+        public string Straat { get; set; }
+        public string Huisnummer { get; set; }
+        public string Gemeente { get; set; }
+        public string Postcode { get; set; }
+
+        public string TelefoonNummer { get; set; }
+        public string GsmNummer { get; set; }
+
+        public string VoorkeursCampus { get; set; }
+        public string VoorkeursOpleiding { get; set; }
+    }
+}
